1936513ac3fa045f932a3c48e0ebbd7ed0ac5f93
Amy <amy.jeanette.morrison@gmail.com>
2024-05-03 22:23:47 +0100

    decrease product quantity in basket page

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -1,12 +1,81 @@
 "use client";
 
+import { formatPrice } from "@/lib/utils";
+import { Trash2 } from "lucide-react";
+import Image from "next/image";
 import { useBasket } from "../store/basket";
+import { Product } from "@/lib/definitions";
+
+interface BasketItem extends Product {
+  quantity: number;
+}
 
 const Page = () => {
-  const { basket } = useBasket();
-  console.log({ basket });
+  const { basket, removeFromBasket, decreaseQuantity } = useBasket();
 
-  return <div>hello world</div>;
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h1 className="text-3xl font-bold sm:text-4xl text-fuchsia-600">
+          Your bag
+        </h1>
+
+        <ul className="divide-y divide-gray-200 border-b border-t border-gray-200">
+          {basket.map(({ product }) => {
+            return (
+              <li key={product.id} className="flex py-6 sm:py-10">
+                <div className="flex-shrink-0">
+                  <div className="relative h-24 w-24">
+                    <Image
+                      fill
+                      src={product.img}
+                      alt="product image"
+                      className="h-full w-full rounded-md object-cover object-center sm:h-48 sm:w-48"
+                    />
+                  </div>
+                </div>
+
+                <div className="ml-4 flex flex-col justify-between sm:ml-6">
+                  <div>
+                    <div>
+                      <div className="flex justify-between">
+                        <h3 className="text-sm font-medium">{product.name}</h3>
+                      </div>
+
+                      <p className="mt-1 text-sm font-medium text-gray-900">
+                        {formatPrice(product.price)}
+                      </p>
+                    </div>
+                  </div>
+
+                  <p className="mt-4 flex space-x-2 text-sm text-gray-700">
+                    <span>Quanity</span>
+                    {/* TODO change to minus */}
+                    <button
+                      onClick={() => decreaseQuantity(product.id)}
+                      aria-label="Decrease quantity"
+                    >
+                      <Trash2 color="red" />
+                    </button>
+                    <span>{product.quantity}</span>
+                  </p>
+                  {/* TODO IS THIS ACCESSIBLE?? */}
+                  <div className="mt-4 ">
+                    <button
+                      onClick={() => removeFromBasket(product.id)}
+                      aria-label="Remove product from basket"
+                    >
+                      <Trash2 color="red" />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/app/store/basket.ts
+++ src/app/store/basket.ts
@@ -1,14 +1,19 @@
-
 import { Product } from "@/lib/definitions";
 import { create } from "zustand";
 
+interface BasketItem extends Product {
+  quantity: number;
+}
+
 export type Item = {
-  product: Product;
+  product: BasketItem;
 };
 
 type BasketState = {
   basket: Item[];
   addToBasket: (product: Product) => void;
+  removeFromBasket: (productId: number) => void;
+  decreaseQuantity: (productId: number) => void;
   emptyBasket: () => void;
 };
 
@@ -16,7 +21,40 @@ export const useBasket = create<BasketState>()((set) => ({
   basket: [],
   addToBasket: (product) =>
     set((state) => {
-      return { basket: [...state.basket, { product }] };
+      const existingProductIndex = state.basket.findIndex(
+        (item) => item.product.id === product.id
+      );
+      if (existingProductIndex !== -1) {
+        const updatedBasket = [...state.basket];
+        updatedBasket[existingProductIndex].product.quantity++;
+        return { basket: updatedBasket };
+      } else {
+        return {
+          basket: [...state.basket, { product: { ...product, quantity: 1 } }],
+        };
+      }
     }),
+  decreaseQuantity: (productId) =>
+    set((state) => {
+      // TODO needs refactoring
+      const existingProductIndex = state.basket.findIndex(
+        (item) => item.product.id === productId
+      );
+      const updatedBasket = [...state.basket];
+      const existingQuantity = [...state.basket][existingProductIndex].product
+        .quantity;
+      if (existingQuantity === 1) {
+        return {
+          basket: state.basket.filter((item) => item.product.id !== productId),
+        };
+      } else {
+        updatedBasket[existingProductIndex].product.quantity--;
+        return { basket: updatedBasket };
+      }
+    }),
+  removeFromBasket: (productId) =>
+    set((state) => ({
+      basket: state.basket.filter((item) => item.product.id !== productId),
+    })),
   emptyBasket: () => set({ basket: [] }),
 }));
